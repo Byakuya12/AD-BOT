4f17aff9b967f05c027f2727e9e385a94ce54390
Byakuya12 <92262589+Byakuya12@users.noreply.github.com>
2022-07-01 22:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from config import bot, auth_users
 from telethon import events, Button
-from telethon.tl.types import MessageMediaPhoto
 
 ad = "NULL"
 link_preview = False
-bs = []
-media = None
+bs = None
 
 @bot.on(events.InlineQuery)
 async def handler(event):
@@ -13,32 +11,24 @@ async def handler(event):
         return
     builder = event.builder
     options = []
-    if media == None:
+    if bs == None:
         options.append(
             builder.article(
                 title = ad, 
                 text = ad, 
                 link_preview = link_preview,
-                buttons=bs
-            )
-        )
-    elif type(media) == MessageMediaPhoto:
-        options.append(
-            builder.photo(
-                text = ad, 
-                link_preview = link_preview,
-                file = media,
-                buttons=bs
             )
         )
     else:
         options.append(
-            builder.document(
+            builder.article(
                 title = ad, 
                 text = ad, 
                 link_preview = link_preview,
-                file = media,
-                buttons=bs
+                buttons=Button.url(
+                    text=bs[0], 
+                    url=bs[1]
+                )
             )
         )
     await event.answer(options)
@@ -49,9 +39,7 @@ async def _(event):
         return
     reply = await event.get_reply_message()
     global ad
-    global media
     ad = reply.text
-    media = reply.media
     await reply.reply("set as current ad")
 
 @bot.on(events.NewMessage(pattern="/link_preview"))
@@ -68,38 +56,20 @@ async def _(event):
         return
     global bs
     try:
-        b1 = event.text.split("|")[1]
-        b2 = event.text.split("|")[2]
-        bs = [[Button.url(text=b1, url=b2)]]
-        await event.reply(f"Button Text: {b1}\nButton URL: {b2}")
+        bs = [event.text.split("|")[1], event.text.split("|")[2]]
+        await event.reply(f"Button Text: {bs[0]}\nButton URL: {bs[1]}")
     except:
-        bs = []
+        bs = None
         await event.reply(f"Button Removed")
-
-@bot.on(events.NewMessage(pattern="/addbutton"))
-async def _(event):
-    if event.sender_id not in auth_users:
-        return
-    global bs
-    try:
-        b1 = event.text.split("|")[1]
-        b2 = event.text.split("|")[2]
-        bs.append([Button.url(text=b1, url=b2)])
-        await event.reply(f"Button Text: {b1}\nButton URL: {b2}")
-
-    except:
-        await event.reply("Error, check format")
-
 
 @bot.on(events.NewMessage(pattern="/resetad"))
 async def _(event):
     if event.sender_id not in auth_users:
         return
-    global ad, bs, link_preview, media
+    global ad, bs, link_preview
     ad = "NULL"
     link_preview = False
     bs = None
-    media = None
     await event.reply("Reset Successful")
 
 bot.start()
